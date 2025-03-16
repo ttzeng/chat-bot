b3ec9be9b1f335a359dc60714997e595f7d4d3a8
Tonny Tzeng <tonny.tzeng@gmail.com>
2025-03-16 10:43:46 +0800

    Migrate to Gemini 2.0 to avoid future errors Signed-off-by: <tonny.tzeng@gmail.com> Close: #13

--- chat_gemini.py
+++ chat_gemini.py
@@ -10,7 +10,7 @@ import google.generativeai as genai
 genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 
 def get_response (prompt: str, image: bytes = None,
-                  model: str = 'gemini-1.5-flash'):
+                  model: str = 'gemini-2.0-flash-lite'):
     # Select the model
     model = genai.GenerativeModel(model)
 
